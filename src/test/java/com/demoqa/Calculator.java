3f41491630e3abe2f50a930186fac69007a8be55
Dmitrii Tuchs <dtuchs@gmail.com>
2023-05-27 00:03:35 +0600

    junit demo

---
+++ src/test/java/com/demoqa/Calculator.java
@@ -0,0 +1,13 @@
+package com.demoqa;
+
+public class Calculator {
+
+  public int sum(int a, int b) {
+    return a + b;
+  }
+
+  public int mult(int a, int b) {
+    return a * b;
+  }
+
+}
---
+++ src/test/java/com/demoqa/DemoJUnitTest.java
@@ -0,0 +1,51 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DemoJUnitTest {
+
+  Calculator calc;
+
+  @BeforeAll
+  static void beforeAll() {
+    System.out.println("#### @BeforeAll");
+//    Configuration.browser = "chrome";
+  }
+
+  @AfterAll
+  static void afterAll() {
+    System.out.println("#### @AfterAll");
+//    Configuration.browser = "chrome";
+  }
+
+  @BeforeEach
+  void beforeEach() {
+    System.out.println("  #### @BeforeEach");
+    calc = new Calculator();
+  }
+
+  @AfterEach
+  void afterEach() {
+    System.out.println("  #### @AfterEach");
+  }
+
+  @Test
+  void firstTest() {
+    System.out.println("    #### @Test firstTest");
+    int result = calc.sum(2, 2);
+    Assertions.assertTrue(result == 4);
+  }
+
+  @Test
+  void secondTest() {
+    System.out.println("    #### @Test secondTest");
+    int result = calc.mult(2, 2);
+    Assertions.assertTrue(result == 4);
+  }
+
+}
